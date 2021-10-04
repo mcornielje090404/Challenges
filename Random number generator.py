beadb6a762d063b34876c7ed02ff1e8b93211b46
mcornielje090404 <90604542+mcornielje090404@users.noreply.github.com>
2021-10-04 11:36:40 +0100

    Random Number Generator

---
+++ Random number generator.py
@@ -0,0 +1,8 @@
+## Random number generator
+import random
+repeat = True
+
+while repeat == True:
+    randomNumber = random.randrange(1, 100)
+    print(randomNumber)
+    input("Press ENTER to generate a new number")
